9f1b7b08457f44e9098901b34aef79f011f0d94c
Rahul Walia <RWalia@ad.prolifics.com>
2019-08-05 15:55:20 +0530

    sample changes for commit

---
+++ Demo-XOM/src/com/dev/DemoInput.java
@@ -0,0 +1,21 @@
+package com.dev;
+
+public class DemoInput {
+private int num=0;
+
+/**
+ * @return the num
+ */
+public int getNum() {
+	return num;
+}
+
+/**
+ * @param num the num to set
+ */
+public void setNum(int num) {
+	this.num = num;
+}
+
+
+}
---
+++ Demo-XOM/src/com/dev/DemoOutput.java
@@ -0,0 +1,21 @@
+package com.dev;
+
+public class DemoOutput {
+private int answer=0;
+
+/**
+ * @return the answer
+ */
+public int getAnswer() {
+	return answer;
+}
+
+/**
+ * @param answer the answer to set
+ */
+public void setAnswer(int answer) {
+	this.answer = answer;
+}
+
+
+}
